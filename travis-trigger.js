18582eb223a1f842c5a8d8b345c60bf98a42eb31
gaudi <craig@resin.io>
2016-10-31 10:53:51 +0200

    init

---
+++ travis-trigger.js
@@ -0,0 +1,30 @@
+const request = require('request');
+
+var options = {
+  url: 'https://api.github.com/repos/craig-mulligan/travis-slave/request',
+  method: 'post',
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Travis-API-Version": "3",
+    "Authorization": 'token ' + process.env.TRAVIS_API_TOKEN,
+  },
+  body: JSON.stringify({
+    request: {
+      message: 'Trigger build',
+      branch: 'source',
+    },
+  }),
+  json: true
+};
+
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log("Triggered build of slave repo");
+  } else {
+    console.log('error', error)
+    process.exit(-1);
+  }
+}
+
+request(options, callback)
